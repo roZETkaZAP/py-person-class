acd2752f6b42c1dffef6a15969d24a899e850d0c
Igor <zalugin.iv2@gmail.com>
2026-03-02 12:08:14 +0100

    added class Person and function create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    result = []
+    for person in people:
+        p = Person(person["name"], person["age"])
+        result.append(p)
+    for pep_data in people:
+        pers = Person.people[pep_data["name"]]
+        wife_name = pep_data.get("wife")
+        if wife_name:
+            pers.wife = Person.people[wife_name]
+        husband_name = pep_data.get("husband")
+        if husband_name:
+            pers.husband = Person.people[husband_name]
+    return result

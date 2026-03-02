a90ac239e3b2d903cf911c45bf0d4df7180ed609
Igor <zalugin.iv2@gmail.com>
2026-03-02 13:17:40 +0100

    added requested chages (Person.people.clear())

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people):
+def create_person_list(people: dict) -> list:
+    Person.people.clear()
     result = [Person(person["name"], person["age"]) for person in people]
     for pep_data in people:
         pers = Person.people[pep_data["name"]]
